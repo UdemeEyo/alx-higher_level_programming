383c8b44e5181a8735ea16ff8dee0b22fc309012
UdemeEyo <udemeeyo2013@gmail.com>
2023-09-12 09:21:11 +0000

    10-square.py

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+A class Rectangle
+"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    A class that inherit from BaseGeometry
+    """
+
+    def __init__(self, size):
+        """
+        The constructor function
+        """
+        self.__size = size
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+
+    def area(self):
+        """
+        Returns the area of a square
+        """
+        return self.__size ** 2

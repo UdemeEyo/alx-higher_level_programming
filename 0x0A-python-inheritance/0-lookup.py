2188c68099d3bc0a1b01f11050e16090b3981884
UdemeEyo <udemeeyo2013@gmail.com>
2023-09-12 01:05:35 -0700

    0-lookup.py

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+A method for getting list of attributes
+by Udeme Eyo
+"""
+
+
+def lookup(obj):
+    """
+    Returns the list of available attributes and method
+    """
+    return dir(obj)
